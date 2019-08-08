f82ca40bcb44b04d1a4e186e289f107ae049ab11
gemah <hianneiva@gmail.com>
2019-08-08 18:11:35 -0300

    Atualização do Script
    Atualização de Script para resolver o problema de atributos repetidos.

--- poc-CTe-XSD2CS/xsdJob.bat
+++ poc-CTe-XSD2CS/xsdJob.bat
@@ -9,4 +9,10 @@ cd ..\schema
 echo Generating POC#O (Plain Old C# Objects) from Schema
 echo WARNING: This Script currently relies on xsd.exe from Win10 .NET 4.6.1
 
-for /R %%S in (\*.xsd) do "%programfiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe" %%S xmldsig-core-schema_v1.01.xsd /c /o:..\model /n:CTe.v300 /l:cs /edb
+SET index=1
+SETLOCAL ENABLEDELAYEDEXPANSION
+for /R %%S in (\*.xsd) do (
+   "%programfiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe" %%S xmldsig-core-schema_v1.01.xsd /c /o:..\model /n:CTe.v!index! /l:cs /edb
+   SET /A index=!index!+1
+)
+SETLOCAL DISABLEDELAYEDEXPANSION
