080ecb0d6c90db0907233e36976853c10644f68f
gemah <hianneiva@gmail.com>
2019-08-09 09:10:14 -0300

    Alteração de script
    Altera o namespace gerado nas classes para refletir a versão dos Schema XSD do CT-e, enquanto mantém a independência entre as classes geradas.

--- poc-CTe-XSD2CS/xsdJob.bat
+++ poc-CTe-XSD2CS/xsdJob.bat
@@ -12,7 +12,7 @@ echo WARNING: This Script currently relies on xsd.exe from Win10 .NET 4.6.1
 SET index=1
 SETLOCAL ENABLEDELAYEDEXPANSION
 for /R %%S in (\*.xsd) do (
-   "%programfiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe" %%S xmldsig-core-schema_v1.01.xsd /c /o:..\model /n:CTe.v!index! /l:cs /edb
+   "%programfiles(x86)%\Microsoft SDKs\Windows\v10.0A\bin\NETFX 4.6.1 Tools\xsd.exe" %%S xmldsig-core-schema_v1.01.xsd /c /o:..\model /n:CTe.v3.00-!index! /l:cs /edb
    SET /A index=!index!+1
 )
 SETLOCAL DISABLEDELAYEDEXPANSION
